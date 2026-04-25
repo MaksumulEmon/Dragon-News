455f6d72bc102f3bc5116565a0cb8146902ca336
MaksumulEmon <maksumulemon300@gmail.com>
2026-04-25 15:43:10 +0600

    Design Layout, FetchCategories

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,10 +1,42 @@
 import Image from "next/image";
 
-export default function Home() {
+async function getCategories() {
+  const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+  const data = await res.json();
+  return data.data;
+}
+
+export default async function Home() {
+  const categories = await getCategories();
+  console.log(categories.news_category)
   return (
-   
-   <>
-   <h2 className="bg-amber-500">Emon</h2>
-   </>
+
+    <div className="grid grid-cols-12 gap-4 my-20">
+
+      <div className="col-span-3">
+       <p className="text-2xl text-bold font-bold"> All Cateroies</p>
+        <ul className="flex flex-col gap-3 mt-4">
+          {
+            categories.news_category.map((category, ind) => {
+              return <li key={ind} className="bg-slate-100 p-2 rounded-md font-bold text-center text-lg">{category.category_name} </li>
+            })
+          }
+        </ul>
+
+      </div>
+
+
+      <div className="bg-green-700 col-span-6">
+        gfhgdfh
+      </div>
+
+
+      <div className="bg-red-800 col-span-3">
+        fghgfh
+      </div>
+
+
+
+    </div>
   );
 }
